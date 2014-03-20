0f69712bc62d32299fa1b7a8624679ab51024f6b
Bill Hathaway <william_hathaway@cable.comcast.com>
2014-03-20 13:12:09 -0700

    stripped down to be a single binary and no other files

--- README.txt
+++ README.txt
@@ -2,9 +2,16 @@ ipecho is a service to return a caller's IP and hostname.
 
 This is useful in cases where your host is behind a NAT or in an environment like OpenStack where it may not be able to determine locally what the external IP address is.
 
-Endpoints:
-/     - renders main page
-/text - returns just IP address with no other formatting
-/json - returns IP and hostname in JSON
+Endpoint
+/ - returns the IP address on a single line
 
+By default the process listens on 0.0.0.0:8080
 
+To change the listen address or port, use
+-host=<IP or name>
+-port=< port>
+
+Example:
+./ipecho -host 127.0.0.1 -port 8888
+
+No logs are emitted except in the case that it is not able to bind to a port, in which case an error is printed to stderr.
--- main.go
+++ main.go
@@ -2,105 +2,50 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"html/template"
 	"io"
 	"net"
 	"net/http"
+	"os"
 	"time"
 )
 
 const (
-	defaultPort          = "8080"
-	defaultHost          = "0.0.0.0"
-	mainPageTemplateFile = "templates/mainPage.tmpl"
+	defaultPort = "8080"
+	defaultHost = "0.0.0.0"
 )
-
-type (
-	clientInfo struct {
-		IP       string
-		Hostname string
-	}
-)
-
-var (
-	mainPageTemplate = template.Must(template.ParseFiles(mainPageTemplateFile))
-)
-
-func log(message string) {
-	fmt.Println(time.Now().Format(time.RFC3339) + " " + message)
-}
-
-func resolve(remoteAddress string) (*clientInfo, error) {
-	client := new(clientInfo)
-	client.Hostname = "UNRESOLVED"
-	var err error
-	client.IP, _, err = net.SplitHostPort(remoteAddress)
-	if err != nil {
-		return client, err
-	}
-	names, err := net.LookupAddr(client.IP)
-	if err != nil {
-		return client, err
-	}
-	if len(names) > 0 {
-		client.Hostname = names[0]
-	}
-	return client, nil
-}
-
-func logRequest(path string, client *clientInfo, responseTime time.Duration, resolveErr error, writeErr error) {
-	buf := bytes.Buffer{}
-	buf.WriteString(fmt.Sprintf("path=%s ip=%s name=%s time=%f", path, client.IP, client.Hostname, responseTime.Seconds()))
-	if resolveErr != nil {
-		buf.WriteString(" resolveErr=" + resolveErr.Error())
-	}
-	if writeErr != nil {
-		buf.WriteString(" writeErr=" + writeErr.Error())
-	}
-	log(buf.String())
-}
 
 func render(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
-	client, resolveErr := resolve(r.RemoteAddr)
-	var writeErr error
-	switch r.URL.Path {
-	default: // for any page that we don't handle explicitly, render the main page
-		fallthrough
-	case "/":
-		writeErr = mainPageTemplate.Execute(w, client)
-	case "/text":
-		_, writeErr = io.WriteString(w, client.IP)
-	case "/json":
-		output, _ := json.Marshal(client)
-		_, writeErr = w.Write(output)
-	case "/favicon.ico":
-		http.ServeFile(w, r, "public/icons/favicon.ico")
-		return // don't bother logging these
+	if r.URL.Path == "/" {
+		w.Header().Set("Content-Type", "text/plain")
+		address, _, _ := net.SplitHostPort(r.RemoteAddr)
+		io.WriteString(w, address+"\n")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
 	}
-
-	logRequest(r.URL.Path, client, time.Since(startTime), resolveErr, writeErr)
 }
 
 func main() {
 	var listenPort string
 	var listenHost string
+	var help bool
 
 	flag.StringVar(&listenPort, "port", defaultPort, "TCP port to listen on")
 	flag.StringVar(&listenHost, "host", defaultHost, "TCP address to listen on")
+	flag.BoolVar(&help, "help", false, "show usage message")
 	flag.Parse()
 
 	listenAddress := net.JoinHostPort(listenHost, listenPort)
 	server := &http.Server{
 		Addr:        listenAddress,
-		ReadTimeout: 10 * time.Second,
+		ReadTimeout: 1 * time.Second,
 	}
 	http.HandleFunc("/", render)
-	log(fmt.Sprintf("event=start address=%s", listenAddress))
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Problem listening: %s\n", err.Error())
+		os.Exit(1)
+	}
 
 }
